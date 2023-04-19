9f19b5f4f3e2f8475727e32ea64be42f8ef09a07
anusha <anushag2k16@gmail.com>
2023-04-19 21:12:15 +0530

    committing to learn-shell

--- function.sh
+++ function.sh
@@ -6,4 +6,31 @@ function_name()
   echo $*
   echo $#
 }
+function_name2()
+{
+  echo $0
+  echo $1
+  }
 function_name anusha learning devops and got placed with 35LPA
+function_name2 "anusha learning devops and got placed with 35LPA"
+# A name given to set of commands is called as function
+
+# Declare a function
+
+function_name() {
+  echo Hello World
+}
+
+# Call function
+function_name
+
+## We can send inputs to the function and we can access them with special variables $1-$n, $*, $#
+
+function_name1() {
+  echo First Argument = $1
+  echo Second Argument = $2
+  echo All Arguments = $*
+  echo No Of Arguments = $#
+}
+
+function_name1 123 xyz
