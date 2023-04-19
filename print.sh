231d17cb25b04edacde153b1439a06ca87894d32
anusha <anushag2k16@gmail.com>
2023-04-19 20:55:58 +0530

    committing to learn-shell

--- print.sh
+++ print.sh
@@ -1,7 +1,16 @@
-#syntax:echo -e /e[COLmcontent/e[0m
-echo -e "/e[34m<<<anusha>>>/e[0m"
-echo -e "/e[31m<<govindaswamy>>>/e[0m"
+#syntax:echo -e \e[COLmcontent\e[0m
 
-echo -e "/e[33m devops /e[0m"
+# Enable Color
+# Syntax - echo -e "\e[COLmMESSAGE\e[0m"
+# -e - enable \e (Enable color in echo command)
+# All the input should be there in quotes (Double quotes preferred)
+# \e - Enable a color
+# COLm - Color Code - Red(31m), Green(32m), Yellow(33m), Blue(34m), Magenta(35m), Cyan(36m)
+# \e[0m - Disable the color. (Once we enable the color we have to diable that, else color folloows to next lines)
+
+echo -e "\e[31mRED COLOR\e[0m"
+echo -e "\e[33manusha\e[0m"
+echo -e "\e[35mgovindaswamy\e[0m"
+echo -e "\e[35mdevops\e[0m"
 
 
