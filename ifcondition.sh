be2e768a0ddbeb01bdb3350fe21116148003cfff
anusha <anushag2k16@gmail.com>
2023-04-20 18:10:25 +0530

    committing to learn-shell

--- ifcondition.sh
+++ ifcondition.sh
@@ -1,17 +1,18 @@
 weather=$(curl -L  https://weatherstack.com/?utm_source=google|grep -w lat| xargs -n1|sed -n '2p'
 )
-  if [ weather gt 0]
+  if [ weather gt 0] then
   echo weather is displaying value
   fi
-  if [ weather !=0]
+  if [ weather !=0] then
   echo weather is giving valuesud
   else [ weather ge 0]
   echo ${weather}
   fi
   input=data
-  if [ -z input ]
+  if [ -z input ] then
   echo input missing
   exit
   fi
+
    echo -e "\e[35m <<<<hjkuiop>>>> \e[0m"
    echo ${weather}
