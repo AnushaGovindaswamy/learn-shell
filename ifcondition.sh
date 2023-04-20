a69b10e42d042edc522fcdf2d757aef050c69e57
anusha <anushag2k16@gmail.com>
2023-04-20 18:16:44 +0530

    committing to learn-shell

--- ifcondition.sh
+++ ifcondition.sh
@@ -1,15 +1,26 @@
+
+fruit_name=$1
+quantity=$2
+
+if [ -z "$fruit_name" ]; then
+  echo Input Missing
+  exit
+fi
 weather=$(curl -L  https://weatherstack.com/?utm_source=google|grep -w lat| xargs -n1|sed -n '2p'
 )
-  if [ weather gt 0] then
+  if [ $weather gt 0]
+   then
   echo weather is displaying value
   fi
-  if [ weather !=0] then
+  if [ $weather !=0]
+  then
   echo weather is giving valuesud
-  else [ weather ge 0]
+  else [ $weather ge 0]
   echo ${weather}
   fi
   input=data
-  if [ -z input ] then
+  if [ -z $input ]
+   then
   echo input missing
   exit
   fi
