3710b7bb432a98fba82d069273099b3083502b4a
anusha <anushag2k16@gmail.com>
2023-04-22 15:44:27 +0530

    committing to learn-shell

---
+++ case.sh
@@ -0,0 +1,11 @@
+fruit=$1
+case $fruit in
+  mango)
+    echo price 100
+    ;;
+  banana)
+    echo price 150
+    ;;
+  *)
+    echo price not found
+    esac
--- print.sh
+++ print.sh
@@ -7,12 +7,13 @@
 # \e - Enable a color
 # COLm - Color Code - Red(31m), Green(32m), Yellow(33m), Blue(34m), Magenta(35m), Cyan(36m)
 # \e[0m - Disable the color. (Once we enable the color we have to diable that, else color folloows to next lines)
-
+#Enable tab -\t
+#to print in next line-\n
 echo -e "\e[31mRED COLOR\e[0m"
 echo -e "\e[33manusha\e[0m"
 echo -e "\e[35mgovindaswamy\e[0m"
 echo -e "\e[35mdevops\e[0m"
-echo -e "Iam\noptimistic\person\12345"
-echo -e "hey\tdo\n\nyou\know\t\tdevops"
+echo -e "Iam\noptimistic\nperson\n12345"
+echo -e "hey\tdo\n\nyou\nknow\t\tdevops"
 
 
