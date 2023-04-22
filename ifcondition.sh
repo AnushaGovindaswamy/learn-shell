ac7d8db3e20ee44541bca024f80ad0d6ec77bc67
anusha <anushag2k16@gmail.com>
2023-04-22 11:01:05 +0530

    committing to learn-shell

--- ifcondition.sh
+++ ifcondition.sh
@@ -8,7 +8,13 @@ fi
 
 weather=$(curl -L  https://weatherstack.com/?utm_source=google|grep -w lat| xargs -n1|sed -n '2p'
 )
+  echo -e "\e[31m <<<<anusha>>>> \e[0m"
+     echo ${weather}
+if [ -z "$quantity" ]
+then
+  echo Quantity missing
+  exit
+  fi
 
-
-   echo -e "\e[35m <<<<hjkuiop>>>> \e[0m"
+   echo -e "\e[35m <<<<govindaswamy>>>> \e[0m"
    echo ${weather}
