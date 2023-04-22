7f070837a0d8736de9c26d92d848b81f523c2505
anusha <anushag2k16@gmail.com>
2023-04-22 10:56:21 +0530

    committing to learn-shell

--- ifcondition.sh
+++ ifcondition.sh
@@ -6,19 +6,10 @@ if [ -z "$fruit_name" ]; then
   echo Input Missing by me
 
 fi
+
 weather=$(curl -L  https://weatherstack.com/?utm_source=google|grep -w lat| xargs -n1|sed -n '2p'
 )
 
-  if [ "$weather" -gt "0"]
-  then
-  echo ${weather} 23
-  fi
-  input=$3
-  if [ -z "$input" ]
-   then
-  echo input missing:
-
-  fi
 
    echo -e "\e[35m <<<<hjkuiop>>>> \e[0m"
    echo ${weather}
