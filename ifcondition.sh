fbba8e78579a616f95e755af1b4557ee3c8c9f4d
anusha <anushag2k16@gmail.com>
2023-04-22 11:15:55 +0530

    committing to learn-shell

--- ifcondition.sh
+++ ifcondition.sh
@@ -5,27 +5,9 @@ if [ -z "$fruit_name" ]; then
   echo Input Missing
   exit
 fi
-if [ "$fruitname" == "mango"]
-then
-echo allow
-else if [ "$fruitname" == "banana"]
-then
-echo partially allow
-else
-  echo dont allow
-  fi
-
 
 weather=$(curl -L  https://weatherstack.com/?utm_source=google|grep -w lat| xargs -n1|sed -n '2p'
 )
   echo -e "\e[31m <<<<anusha>>>> \e[0m"
      echo ${weather}
-if [ -z "$quantity" ]
-then
-  echo Quantity missing
-  exit
-  fi
-  if [ "$quantity" -gt 100 ]
-  then
-    echo profit
-  fi
+
