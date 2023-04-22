149a51f57b5de7696163324daf2cd40e05f0d41d
anusha <anushag2k16@gmail.com>
2023-04-22 10:58:30 +0530

    committing to learn-shell

--- ifcondition.sh
+++ ifcondition.sh
@@ -1,10 +1,9 @@
-
 fruit_name=$1
 quantity=$2
 
 if [ -z "$fruit_name" ]; then
-  echo Input Missing by me
-
+  echo Input Missing
+  exit
 fi
 
 weather=$(curl -L  https://weatherstack.com/?utm_source=google|grep -w lat| xargs -n1|sed -n '2p'
